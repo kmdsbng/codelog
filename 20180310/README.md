cf3346ca186a82c74028e4994174932f481fd47f
Yoshihiro Kameda <kameda.sbng@gmail.com>
2018-03-11 11:11:01 +0900

    add codes

---
+++ 20180310/README.md
@@ -0,0 +1,10 @@
+
+// Elvis operator
+val lb3 = b?.length ?: -1
+
+// filterNotNull
+val nullableList: List<Int?> = listOf(1, 2, null, 4)
+val intList: List<Int> = nullableList.filterNotNull()
+
+
+
---
+++ 20180310/null_safety/src/main/kotlin/App.kt
@@ -0,0 +1,29 @@
+fun main(argv: Array<String>) {
+
+    var a: String = "abc"
+    //a = null // compilation error
+
+    var b: String? = "abc"
+    b = null // ok
+
+    val l = a.length
+
+    //val lb = b.length // error: variable 'b' can be null
+
+
+    if (b != null && b.length > 0) {
+        println("String of length ${b.length}")
+    } else {
+        println("Empty string")
+    }
+    val lb2 = b?.length // error: variable 'b' can be null
+    println(lb2)
+
+    // Elvis operator
+    val lb3 = b?.length ?: -1
+
+    println(lb3)
+
+    val nullableList: List<Int?> = listOf(1, 2, null, 4)
+    val nonnullList = nullableList.filterNotNull()
+}
